d85a11bdf66b7f4dc6319c4670fb02566095d1b4
Henry Wilder <henrythepony@gmail.com>
2024-03-05 21:18:19 -0500

    Added reverse example

--- example/example.cpp
+++ example/example.cpp
@@ -10,9 +10,25 @@ int main()
 	graph.push("World!");
 	graph.link(0, 1, ", ");
 
+	// Forward first
+	// "Hello, World!"
+
 	for (auto [v,e] : graph.walk<BREADTH_FIRST>(0))
 	{
 		if (e) std::cout << **e;
 		if (v) std::cout << **v;
 	}
+
+	std::cout << '\n';
+
+	// Backward second
+	// "World!, Hello"
+
+	for (auto [v, e] : graph.walk<BREADTH_FIRST, BACKWARD>(1))
+	{
+		if (e) std::cout << **e;
+		if (v) std::cout << **v;
+	}
+
+	std::cout << '\n';
 }
