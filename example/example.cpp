ba5b2cf85f1b92e97de0f97dee21f9ef87ae0eed
Henry Wilder <henrythepony@gmail.com>
2024-03-06 13:45:43 -0500

    Made edges voidable

--- example/example.cpp
+++ example/example.cpp
@@ -5,33 +5,74 @@ using namespace trav;
 
 int main()
 {
-	graph<const char*, const char*> graph;
-	graph.push("v0");
-	graph.push("v1");
-	graph.link(0, 1, "e0-1");
-	graph.push("v2");
-	graph.link(0, 2, "e0-2");
-	graph.push("v3");
-	graph.link(1, 3, "e1-3");
-	graph.link(2, 3, "e2-3");
-
-	// Forward first
-	// "Hello, World!"
-
-	for (auto &[e, v] : graph.walk_bfs(0))
 	{
-		std::cout << (e ? *e : "") << ' ' << v << ' ';
+		graph<const char *> graph;
+		graph.push("v0");
+		graph.push("v1");
+		graph.link(0, 1);
+		graph.push("v2");
+		graph.link(0, 2);
+		graph.push("v3");
+		graph.link(1, 3);
+		graph.link(2, 3);
+
+		// Forward first
+
+		auto walk = graph.walk_bfs({ 0, 2 });
+
+		for (auto &[e, v] : walk)
+		{
+			std::cout << v << ' ';
+		}
+
+		std::cout << '\n';
+
+		for (auto &[e, v] : walk)
+		{
+			std::cout << v << ' ';
+		}
+
+		std::cout << '\n';
+
+		// Backward second
+
+		for (auto &[e, v] : graph.walk_bfs_r(3))
+		{
+			std::cout << v << ' ';
+		}
+
+		std::cout << '\n';
 	}
 
-	std::cout << '\n';
-
-	// Backward second
-	// "World!, Hello"
-
-	for (auto &[e, v] : graph.walk_bfs_r(3))
 	{
-		std::cout << (e ? *e : "") << ' ' << v << ' ';
-	}
+		graph<const char *, const char *> graph;
+		graph.push("v0");
+		graph.push("v1");
+		graph.link(0, 1, "e0-1");
+		graph.push("v2");
+		graph.link(0, 2, "e0-2");
+		graph.push("v3");
+		graph.link(1, 3, "e1-3");
+		graph.link(2, 3, "e2-3");
 
-	std::cout << '\n';
+		// Forward first
+
+		for (auto &[e, v] : graph.walk_bfs({ 0, 2 }))
+		{
+			if (e) std::cout << *e << ' ';
+			std::cout << v << ' ';
+		}
+
+		std::cout << '\n';
+
+		// Backward second
+
+		for (auto &[e, v] : graph.walk_bfs_r(3))
+		{
+			if (e) std::cout << *e << ' ';
+			std::cout << v << ' ';
+		}
+
+		std::cout << '\n';
+	}
 }
--- template-traversal-library/graph-traversal.hpp
+++ template-traversal-library/graph-traversal.hpp
@@ -2,58 +2,92 @@
 #include <type_traits>
 #include <cassert>
 #include <queue>
-#include <set>
-#include <unordered_set>
 #include <vector>
 #include <functional>
 #include <tuple>
 #include <optional>
 
+#define _In_vector_index_(vec) _In_range_(0, vec.size( ) - 1)
+
 // graph traversal
 namespace trav
 {
-    template<class _VTy, class _ETy> class graph;
+    template<class _Ty> concept non_void = !std::is_void_v<_Ty>;
 
     template<class _VTy>
     class vert
     {
     public:
-        template<class _VTy, class _ETy> friend class graph;
-
-        vert(_In_ size_t index, _In_ const _VTy &data)
-            : index(index), data(data)
+        vert(
+            _In_ size_t index,
+            _In_ const _VTy &data
+            ) :
+            _index(index),
+            _data(data)
         { }
 
-        size_t GetIndex( ) const { return index; }
+        size_t index( ) const { return _index; }
 
-        operator       _VTy &( )       { return data; }
-        operator const _VTy &( ) const { return data; }
+        using edge_container = std::vector<size_t>;
+
+              edge_container &prev( )       { return _prev; }
+        const edge_container &prev( ) const { return _prev; }
+              edge_container &next( )       { return _next; }
+        const edge_container &next( ) const { return _next; }
+
+        operator       _VTy &( )       { return _data; }
+        operator const _VTy &( ) const { return _data; }
 
     private:
-        size_t index;
-        std::set<size_t> adj;
-        _VTy data;
+        size_t _index; // verts
+        edge_container _prev, _next; // edges
+        _VTy _data;
     };
 
-    template<class _ETy>
-    class edge
+    template<class _ETy = void> class edge;
+
+    template<>
+    class edge<void>
     {
     public:
-        template<class _VTy, class _ETy> friend class graph;
-
-        edge(_In_ size_t index, _In_ size_t prev, _In_ size_t next, _In_ const _ETy &data)
-            : _index(index), _prev(prev), _next(next), _data(data)
+        edge(
+            _In_ size_t index,
+            _In_ size_t prev,
+            _In_ size_t next
+            ) :
+            _index(index),
+            _prev(prev),
+            _next(next)
         { }
 
         size_t index( ) const { return _index; }
         size_t prev ( ) const { return _prev;  }
         size_t next ( ) const { return _next;  }
 
+    private:
+        size_t _index; // edges
+        size_t _prev, _next; // verts
+    };
+
+    template<non_void _ETy>
+    class edge<_ETy>
+        : public edge<void>
+    {
+    public:
+        edge(
+            _In_ size_t index,
+            _In_ size_t prev,
+            _In_ size_t next,
+            _In_ const _ETy &data
+            ) :
+            edge<void>(index, prev, next),
+            _data(data)
+        { }
+
         operator       _ETy &( )       { return _data; }
         operator const _ETy &( ) const { return _data; }
 
     private:
-        size_t _index, _prev, _next;
         _ETy _data;
     };
 
@@ -61,137 +95,218 @@ namespace trav
 
     enum class direction { BACKWARD, FORWARD, };
 
-    template<class _VTy, class _ETy>
-    class graph
+    namespace
     {
-    public:
-        using vert = vert<_VTy>;
-        using edge = edge<_ETy>;
-
-        using vert_process = std::function<void(vert &)>;
-        using edge_process = std::function<void(edge &)>;
-
-        static void ignore(vert &) { }
-        static void ignore(edge &) { }
-
-        ~graph( )
-        {
-            for (vert *v : verts) delete v;
-            for (edge *e : edges) delete e;
-        }
-
-        void push(const _VTy &value)
-        {
-            verts.push_back(new vert(verts.size( ), value));
-        }
-
-        vert &at(_In_range_(0, verts.size( ) - 1) size_t index)
-        {
-            return *verts[index];
-        }
-        const vert &at(_In_range_(0, verts.size( ) - 1) size_t index) const
-        {
-            return *verts[index];
-        }
-
-        void link(_In_ size_t prev, _In_ size_t next, _In_ const _ETy &value)
-        {
-            assert(prev != next);
-
-            size_t newEdgeIndex = edges.size( );
-            edges.push_back(new edge(newEdgeIndex, prev, next, value));
-            for (size_t vert : { prev, next })
-            {
-                verts.at(vert)->adj.insert(newEdgeIndex);
-            }
-        }
-
-        edge &link_at(_In_range_(0, edges.size( ) - 1) size_t index)
-        {
-            return *edges[index];
-        }
-        const edge &link_at(_In_range_(0, edges.size( ) - 1) size_t index) const
-        {
-            return *edges[index];
-        }
-
-        template<traversal kind, direction dir>
-        class walker
+        template<class _VTy, class _ETy = void>
+        class _graph_base
         {
         public:
-            walker(_In_ graph &g, _In_ size_t rootIndex)
-                : g(g), rootIndex(rootIndex)
+            using vert = vert<_VTy>;
+            using edge = edge<_ETy>;
+
+            ~_graph_base( )
             {
-                std::queue<size_t> q;
-                std::unordered_set<size_t> visited;
+                for (vert *v : verts) delete v;
+                for (edge *e : edges) delete e;
+            }
 
-                vert *root = g.verts[rootIndex];
-                visited.insert(rootIndex);
-                q.push(rootIndex);
-                _push_to_walk(nullptr, root);
+            void push(
+                _In_ const _VTy &value
+            )
+            {
+                verts.push_back(new vert(verts.size( ), value));
+            }
 
-                while (!q.empty())
+            vert &at(
+                _In_vector_index_(verts) size_t index
+            )
+            {
+                return *verts[index];
+            }
+
+            const vert &at(
+                _In_vector_index_(verts) size_t index
+            ) const
+            {
+                return *verts[index];
+            }
+
+        protected:
+            void _link(
+                _In_ size_t prev,
+                _In_ size_t next,
+                _In_ edge *_edge
+            )
+            {
+                assert(prev < this->verts.size( ));
+                assert(next < this->verts.size( ));
+                assert(prev != next);
+
+                size_t newEdgeIndex = edges.size( );
+                verts[prev]->next( ).push_back(newEdgeIndex);
+                verts[next]->prev( ).push_back(newEdgeIndex);
+                edges.push_back(_edge);
+            }
+
+        public:
+            edge &edge_at(
+                _In_vector_index_(edges) size_t index
+            )
+            {
+                return *edges[index];
+            }
+
+            const edge &edge_at(
+                _In_vector_index_(edges) size_t index
+            ) const
+            {
+                return *edges[index];
+            }
+
+            _Ret_maybenull_ edge *edge_connecting(
+                _In_vector_index_(verts) size_t from_vert,
+                _In_vector_index_(verts) size_t   to_vert
+            )
+            {
+                auto &from_vert_edges = verts[from_vert]->next( );
+                auto &to_vert_edges = verts[to_vert]->prev( );
+
+                if (from_vert_edges.size( ) < to_vert_edges.size( ))
                 {
-                    const size_t vIndex = q.front( );
-                    const vert *v = g.verts[vIndex];
-                    q.pop( );
-
-                    if constexpr (kind == traversal::BREADTH_FIRST)
+                    // Look for 'to' in 'from'
+                    for (size_t edgeIndex : from_vert_edges)
                     {
-                        for (const size_t eIndex : v->adj)
+                        edge *e = edges[edgeIndex];
+                        if (e->next( ) == to_vert) return e;
+                    }
+                }
+                else
+                {
+                    // Look for 'from' in 'to'
+                    for (size_t edgeIndex : to_vert_edges)
+                    {
+                        edge *e = edges[edgeIndex];
+                        if (e->prev( ) == from_vert) return e;
+                    }
+                }
+
+                return nullptr;
+            }
+
+            template<traversal kind, direction dir>
+            class walk
+            {
+            public:
+                walk(
+                    _In_ _graph_base &g,
+                    _In_ std::initializer_list<size_t> rootIndices
+                )
+                    : g(g)
+                {
+                    std::queue<size_t> q;
+                    std::vector<bool> visited(g.verts.size( ), false);
+
+                    auto _visit = [&](
+                        _In_vector_index_(g.verts) size_t vIndex,
+                        _In_opt_ edge *e
+                        )
+                    {
+                        visited[vIndex] = true;
+                        q.push(vIndex);
+                        _push_to_walk(e, g.verts[vIndex]);
+                    };
+
+                    for (size_t rootIndex : rootIndices)
+                    {
+                        _visit(rootIndex, nullptr);
+                    }
+
+                    constexpr bool is_traversal_forward = dir == direction::FORWARD;
+
+                    while (!q.empty( ))
+                    {
+                        const size_t vIndex = q.front( );
+                        const vert *v = g.verts[vIndex];
+                        q.pop( );
+
+                        if constexpr (kind == traversal::BREADTH_FIRST)
                         {
-                            edge *e = g.edges[eIndex];
-                            bool isForward = e->prev( ) == vIndex;
-                            const size_t wIndex = isForward ? e->next( ) : e->prev( );
-                            vert *w = g.verts[wIndex];
-
-                            if (isForward != (dir == direction::FORWARD)) continue;
-
-                            if (!visited.contains(wIndex))
+                            for (const size_t eIndex : (is_traversal_forward ? v->next( ) : v->prev( )))
                             {
-                                visited.insert(wIndex);
-                                q.push(wIndex);
-                                _push_to_walk(e, w);
+                                edge *e = g.edges[eIndex];
+                                const size_t wIndex = (is_traversal_forward ? e->next( ) : e->prev( ));
+
+                                if (!visited[wIndex])
+                                {
+                                    _visit(wIndex, e);
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            auto begin( ) const
-            {
-                return _walk.begin( );
-            }
+                auto begin( ) const { return _walk.begin( ); }
+                auto end  ( ) const { return _walk.end  ( ); }
 
-            auto end( ) const
-            {
-                return _walk.end( );
-            }
+            private:
+                void _push_to_walk(
+                    _In_opt_ edge *e,
+                    _In_     vert *v
+                )
+                {
+                    _walk.push_back({ e, *v });
+                }
 
-        private:
-            void _push_to_walk(_In_opt_ edge *e, _In_ vert *v)
-            {
-                _walk.push_back(std::tuple<edge *, vert &>{ e, *v });
-            }
+                _graph_base &g;
+                std::vector<std::tuple<edge *, vert &>> _walk;
+            };
 
-            graph &g;
-            size_t rootIndex;
-            std::vector<std::tuple<edge *, vert &>> _walk;
+            auto walk_bfs  (_In_ std::initializer_list<size_t> roots) { return walk<traversal::BREADTH_FIRST, direction:: FORWARD>(*this, roots); }
+            auto walk_bfs_r(_In_ std::initializer_list<size_t> roots) { return walk<traversal::BREADTH_FIRST, direction::BACKWARD>(*this, roots); }
+            auto walk_dfs  (_In_ std::initializer_list<size_t> roots) { return walk<traversal::  DEPTH_FIRST, direction:: FORWARD>(*this, roots); }
+            auto walk_dfs_r(_In_ std::initializer_list<size_t> roots) { return walk<traversal::  DEPTH_FIRST, direction::BACKWARD>(*this, roots); }
+    
+            auto walk_bfs  (_In_vector_index_(verts) size_t root) { return walk_bfs  ({ root }); }
+            auto walk_bfs_r(_In_vector_index_(verts) size_t root) { return walk_bfs_r({ root }); }
+            auto walk_dfs  (_In_vector_index_(verts) size_t root) { return walk_dfs  ({ root }); }
+            auto walk_dfs_r(_In_vector_index_(verts) size_t root) { return walk_dfs_r({ root }); }
+
+        protected:
+            std::vector<vert *> verts;
+            std::vector<edge *> edges;
         };
+    }
 
-        template<traversal kind, direction dir = direction::FORWARD>
-        walker<kind, dir> walk(_In_ size_t root)
+    template<class _VTy, class _ETy = void> class graph;
+
+    template<class _VTy>
+    class graph<_VTy, void>
+        : public _graph_base<_VTy, void>
+    {
+    public:
+        void link(
+            _In_ size_t prev,
+            _In_ size_t next
+        )
         {
-            return walker<kind, dir>(*this, root);
+            this->_link(prev, next, new edge<void>(this->edges.size( ), prev, next));
         }
+    };
 
-        auto walk_bfs  (_In_ size_t root) { return walk<traversal::BREADTH_FIRST, direction:: FORWARD>(root); }
-        auto walk_bfs_r(_In_ size_t root) { return walk<traversal::BREADTH_FIRST, direction::BACKWARD>(root); }
-        auto walk_dfs  (_In_ size_t root) { return walk<traversal::  DEPTH_FIRST, direction:: FORWARD>(root); }
-        auto walk_dfs_r(_In_ size_t root) { return walk<traversal::  DEPTH_FIRST, direction::BACKWARD>(root); }
-
-    protected:
-        std::vector<vert *> verts;
-        std::vector<edge *> edges;
+    template<class _VTy, non_void _ETy>
+    class graph<_VTy, _ETy>
+        : public _graph_base<_VTy, _ETy>
+    {
+    public:
+        void link(
+            _In_ size_t prev,
+            _In_ size_t next,
+            _In_ const _ETy &value
+        )
+        {
+            this->_link(prev, next, new edge<_ETy>(this->edges.size(), prev, next, value));
+        }
     };
 }
+
+#undef _In_vector_index_
